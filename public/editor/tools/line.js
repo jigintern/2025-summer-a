31ac36324f5138fcdd130944714d698d0a42c335
Yukkku <yukku.scratcher@gmail.com>
2025-08-25 09:45:48 +0900

    👍 左斜めの線も引けるように拡張

--- public/editor/tools/line.js
+++ public/editor/tools/line.js
@@ -21,7 +21,13 @@ export const line = (aa, width, height, start, stop) => {
       const x1 = Math.round(((i - start[0]) / dy) * dx + start[1]);
       const x2 = Math.round(((i + 1 - start[0]) / dy) * dx + start[1]);
       const lwidth = x2 - x1;
-      if (-3 <= lwidth && lwidth <= 3) {
+      if (5 <= lwidth && lwidth <= 13) {
+        // 罫線 (幅11)
+        cp.addChar(i, Math.round((x1 + x2) / 2 - 8), "＼");
+      } else if (5 <= lwidth && lwidth <= 13) {
+        // 罫線 (幅11)
+        cp.addChar(i, Math.round((x1 + x2) / 2 - 5.5), "╲");
+      } else if (-3 <= lwidth && lwidth <= 5) {
         // ほぼまっすぐなのでパイプ
         cp.addChar(i, Math.round((x1 + x2) / 2 - 2), "|");
       } else if (-9 <= lwidth && lwidth <= -4) {
@@ -29,7 +35,7 @@ export const line = (aa, width, height, start, stop) => {
         cp.addChar(i, Math.round((x1 + x2) / 2 - 4), "/");
       } else if (-13 <= lwidth && lwidth <= -10) {
         // 罫線 (幅11)
-        cp.addChar(i, Math.round((x1 + x2) / 2 - 5), "╱");
+        cp.addChar(i, Math.round((x1 + x2) / 2 - 5.5), "╱");
       } else if (lwidth <= -14) {
         // 全角スラッシュ (幅16)
         cp.addChar(i, Math.round((x1 + x2) / 2 - 8), "／");
