2f47a81f8939460f552c3d00ae398fe5b0cd735d
mepi02 <r122155@g.kobe-kosen.ac.jp>
2025-08-25 13:06:41 +0900

    ✨ add　リダイレクトの追加
    indexだけ入れる

--- public/login/signup.js
+++ public/login/signup.js
@@ -1,33 +1,33 @@
 //サインアップフォームの送信イベントをキャッチ
-document.getElementById("loginForm")
-    .addEventListener("submit", async (e) => {
-        e.preventDefault();
+document.getElementById("signupForm")
+  .addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-        const userName = document.getElementById("username").value;
-        const passWord = document.getElementById("password").value;
+    const userName = document.getElementById("username").value;
+    const passWord = document.getElementById("password").value;
 
-        try {
-            const response = await fetch("/signup", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    userName,
-                    passWord,
-                }),
-            });
+    try {
+      const response = await fetch("/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userName,
+          passWord,
+        }),
+      });
 
-            const result = await response.text();
+      const result = await response.text();
 
-            if (response.ok) {
-                alert("新規作成成功！");
-                window.location.href = "./login.html";
-            } else {
-                alert(result || "新規作成に失敗しました。");
-            }
-        } catch (error) {
-            console.error("エラー:", error);
-            alert("アカウント作成中にエラーが発生しました。");
-        }
-    });
+      if (response.ok) {
+        alert("新規作成成功！");
+        window.location.href = "./login.html";
+      } else {
+        alert(result || "新規作成に失敗しました。");
+      }
+    } catch (error) {
+      console.error("エラー:", error);
+      alert("アカウント作成中にエラーが発生しました。");
+    }
+  });
