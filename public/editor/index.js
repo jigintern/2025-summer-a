3a6963a3161a69170cbb33de6e77fd293bfd6597
RusKRusK <uekitaberuhito@gmail.com>
2025-08-25 15:23:25 +0900

    👍 タイトルに戻る処理を追加

--- public/editor/index.js
+++ public/editor/index.js
@@ -6,6 +6,40 @@ document.addEventListener("DOMContentLoaded", () => {
   const toolButtons = document.querySelectorAll('input[name="tool-button"]');
   const charButtons = document.querySelectorAll('input[name="char-bar"]');
 
+  const newButton = document.getElementById("btn-new");
+  const openButton = document.getElementById("btn-open");
+  const saveButton = document.getElementById("btn-save");
+  const backButton = document.getElementById("btn-back");
+
+  const handleNew = () => {
+    console.log("「新規作成」ボタンが押されました。");
+    // ここに新規作成の処理を書いていく
+  };
+
+  const handleOpen = () => {
+    console.log("「開く」ボタンが押されました。");
+    // ここにファイルを開く処理を書いていく
+  };
+
+  const handleSave = () => {
+    console.log("「保存」ボタンが押されました。");
+    // ここに保存処理を書いていく
+  };
+
+  const handleBack = () => {
+    if (
+      globalThis.confirm("編集中の内容は失われます。タイトルに戻りますか？")
+    ) {
+      globalThis.location.href = "/public/index.html";
+    }
+  };
+
+  // 各ボタンにクリックイベントを設定
+  newButton.addEventListener("click", handleNew);
+  openButton.addEventListener("click", handleOpen);
+  saveButton.addEventListener("click", handleSave);
+  backButton.addEventListener("click", handleBack);
+
   // 選択されている情報を更新して表示する
   const updateDebugInfo = () => {
     // 現在選択されているラジオボタンの要素を取得
