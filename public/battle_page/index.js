94980afcd9b888be39e102f3461e118137e38c61
shirohige11037 <shirohige1025@gmail.com>
2025-08-26 05:05:42 +0900

    ✨ meterのアニメーションを作成

--- public/battle_page/index.js
+++ public/battle_page/index.js
@@ -51,8 +51,86 @@ function getMousePosition(canvas, evt) {
   return [evt.clientX - rect.left, evt.clientY - rect.top];
 }
 
+let metarNum = 0;
+const metarMaxNum = 10;
+const metarVelc = 0.05;
+
+function drawMeter() {
+  ctx.beginPath();
+  ctx.fillStyle = "#898989ff";
+  ctx.moveTo(canvas.width / 5, canvas.height - 20);
+  ctx.lineTo(
+    canvas.width / 5 - canvas.width / 20,
+    canvas.height - 20 - metarMaxNum * 15,
+  );
+  ctx.lineTo(
+    canvas.width / 5 + canvas.width / 20,
+    canvas.height - 20 - metarMaxNum * 15,
+  );
+  ctx.fill();
+  ctx.closePath();
+
+  let num;
+
+  if (metarNum < metarMaxNum / 5) num = metarNum;
+  else num = metarMaxNum / 5;
+
+  ctx.beginPath();
+  ctx.fillStyle = "#bbff00ff";
+  ctx.moveTo(canvas.width / 5, canvas.height - 20);
+  ctx.lineTo(
+    canvas.width / 5 - canvas.width * num / metarMaxNum / 20,
+    canvas.height - 20 - num * 15,
+  );
+  ctx.lineTo(
+    canvas.width / 5 + canvas.width * num / metarMaxNum / 20,
+    canvas.height - 20 - num * 15,
+  );
+  ctx.fill();
+  ctx.closePath();
+
+  const colors = [
+    "#bbff00ff",
+    "#00a6ffff",
+    "#002fffff",
+    "#e600ffff",
+    "#ff002bff",
+  ];
+
+  for (let i = 1; i < 5; i++) {
+    if (metarNum < metarMaxNum / 5 * i) break;
+    if (metarNum > metarMaxNum / 5 * (i + 1)) {
+      num = metarMaxNum / 5 * (i + 1);
+    } else num = metarNum;
+    ctx.beginPath();
+    ctx.fillStyle = colors[i];
+    ctx.moveTo(
+      canvas.width / 5 - canvas.width / 5 * i / 20,
+      canvas.height - 20 - metarMaxNum / 5 * i * 15,
+    );
+    ctx.lineTo(
+      canvas.width / 5 - canvas.width * num / metarMaxNum / 20,
+      canvas.height - 20 - num * 15,
+    );
+    ctx.lineTo(
+      canvas.width / 5 + canvas.width * num / metarMaxNum / 20,
+      canvas.height - 20 - num * 15,
+    );
+    ctx.lineTo(
+      canvas.width / 5 + canvas.width / 5 * i / 20,
+      canvas.height - 20 - metarMaxNum / 5 * i * 15,
+    );
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  metarNum += metarVelc;
+  metarNum %= metarMaxNum;
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawButton();
+  drawMeter();
 }
 setInterval(draw, 10);
