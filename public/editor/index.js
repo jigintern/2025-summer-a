8ad9b20af155cefb260c8d82b2829349668dfdc6
Yukkku <yukku.scratcher@gmail.com>
2025-08-26 12:03:55 +0900

    🎨 デバッグ用の処理を一箇所にまとめた

--- public/editor/index.js
+++ public/editor/index.js
@@ -1,11 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // デバッグ用
-  const debugInfoElement = document.getElementById("debug-info");
-
-  // ラジオボタンのグループを取得
-  const toolButtons = document.querySelectorAll('input[name="tool-button"]');
-  const charButtons = document.querySelectorAll('input[name="char-bar"]');
-
   const newButton = document.getElementById("btn-new");
   const openButton = document.getElementById("btn-open");
   const saveButton = document.getElementById("btn-save");
@@ -69,33 +62,41 @@ document.addEventListener("DOMContentLoaded", () => {
   saveButton.addEventListener("click", handleSave);
   backButton.addEventListener("click", handleBack);
 
-  // 選択されている情報を更新して表示する
-  const updateDebugInfo = () => {
-    // 現在選択されているラジオボタンの要素を取得
-    const selectedTool = document.querySelector(
-      'input[name="tool-button"]:checked',
-    );
-    const selectedChar = document.querySelector(
-      'input[name="char-bar"]:checked',
-    );
+  // デバッグ用処理
+  {
+    const debugInfoElement = document.getElementById("debug-info");
 
-    const selectedToolId = selectedTool ? selectedTool.id : "none";
-    const selectedCharId = selectedChar ? selectedChar.id : "none";
+    // ラジオボタンのグループを取得
+    const toolButtons = document.querySelectorAll('input[name="tool-button"]');
+    const charButtons = document.querySelectorAll('input[name="char-bar"]');
 
-    // デバッグ用
-    debugInfoElement.textContent =
-      `選択中のツール: ${selectedToolId} | 選択中のオプション: ${selectedCharId}`;
-  };
+    // 選択されている情報を更新して表示する
+    const updateDebugInfo = () => {
+      // 現在選択されているラジオボタンの要素を取得
+      const selectedTool = document.querySelector(
+        'input[name="tool-button"]:checked',
+      );
+      const selectedChar = document.querySelector(
+        'input[name="char-bar"]:checked',
+      );
 
-  // 各ボタンにイベントリスナーを設定する
-  toolButtons.forEach((button) => {
-    button.addEventListener("change", updateDebugInfo);
-  });
+      const selectedToolId = selectedTool ? selectedTool.id : "none";
+      const selectedCharId = selectedChar ? selectedChar.id : "none";
 
-  charButtons.forEach((button) => {
-    button.addEventListener("change", updateDebugInfo);
-  });
+      // デバッグ用
+      debugInfoElement.textContent =
+        `選択中のツール: ${selectedToolId} | 選択中のオプション: ${selectedCharId}`;
+    };
 
-  // 一回呼んどくか
-  updateDebugInfo();
+    // 各ボタンにイベントリスナーを設定する
+    toolButtons.forEach((button) => {
+      button.addEventListener("change", updateDebugInfo);
+    });
+
+    charButtons.forEach((button) => {
+      button.addEventListener("change", updateDebugInfo);
+    });
+    // 一回呼んどくか
+    updateDebugInfo();
+  }
 });
