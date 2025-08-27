e00a0dd3ff7629f2e6d9b0c280e83c4f61db690b
Yukkku <yukku.scratcher@gmail.com>
2025-08-27 13:11:07 +0900

    👍 battle.server.jsを作成中と分かるように追記

--- battle.server.js
+++ battle.server.js
@@ -4,4 +4,7 @@
  */
 export const battle = (player1, player2) => {
   // TODO: 対戦を実装する
+  console.log(`対戦開始: ${player1[0]} vs ${player2[0]}`);
+  player1[1].close(1, "まだ作ってない!");
+  player1[2].close(1, "まだ作ってない!");
 };
