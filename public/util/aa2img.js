79d1d803c098274d50f8d63b6532232e23c48a74
Yukkku <yukku.scratcher@gmail.com>
2025-08-28 09:23:38 +0900

    ✨ 中身を実装

---
+++ public/util/aa2img.js
@@ -0,0 +1,22 @@
+import { CharPlace } from "../editor/tools/util.js";
+
+/** @type {OffscreenCanvas} */
+const canvas = new OffscreenCanvas(1, 1);
+/** @type {OffscreenCanvasRenderingContext2D} */
+const ctx = canvas.getContext("2d");
+
+/**
+ * @param {string} aa
+ */
+export const aa2blob = async (aa) => {
+  const [top, right, bottom, left] = new CharPlace(aa, 1000, 720).getRect();
+  canvas.width = right - left;
+  canvas.height = (bottom - top) * 18;
+  ctx.font = "12pt 'MS PGothic'";
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const saa = aa.split("\n");
+  for (let i = 0; i < saa.length; ++i) {
+    ctx.fillText(saa[i], 0 - left, (i - top) * 18 + 15);
+  }
+  return URL.createObjectURL(await canvas.convertToBlob());
+};
